8bed6749a0919d9df2ce46e001886e57ce937d0a
karakuik <wesloke@gmail.com>
2024-04-10 15:06:59 -0400

    Wrapping up the CR of CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,14 +6,14 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
-    
+
     if @article.save
       redirect_to @article
     else
